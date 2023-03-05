cb558af88600a9659d7f33f56b922ca1553993a5
Isadora Moura <isadora-conceicao@hotmail.com>
2023-03-05 10:54:23 -0300

    projeto 1 CTD

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-
-
-while True:
-     idade = int(input('Idade: '))
-     while sexo not in 'MF':
-         sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
-   
-    resp = " "
-    while resp not in 'SN':
-         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    print("Acabou")
-
-new_func()
+velocidade = float(input("Qual a velocidade atual do carro? "))#determinar a velocidade do carro
+if velocidade > 80: #se a velocidade for maior que 80 exibir uma multa
+    print("MULTADO! Você excedeu o limite permitido que é de 80km/h") #mensagem que o individuo recebe a multa
+    multa = (velocidade-80) * 7 #  a multa =(igual) a velocidade -(menos) 80(que é a km permitida) *(vezes) o valor por km/h= 7
+    print("Você deve pagar uma multa de R${:.2f}!".format(multa)) # o resultado é formatado com dois pontos decimais flutuantes
+print("Tenha um bom dia! Dirija com segurança!")
+        
