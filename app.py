e0b9817f6cdceb819e0015b5314c0d144236edcf
Isadora Moura <isadora-conceicao@hotmail.com>
2023-03-05 08:50:14 -0300

    teste 1

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print('Seja bem Vindo!')
+
+
+while True:
+     idade = int(input('Idade: '))
+     while sexo not in 'MF':
+         sexo = str(input('Sexo: [M/F]')).strip().upper()[0]
+   
+    resp = " "
+    while resp not in 'SN':
+         resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    print("Acabou")
+
+new_func()
